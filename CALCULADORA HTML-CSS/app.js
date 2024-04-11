a65dfe2f0f67cd8f35e08497acf3fcf9e6b76176
diegoc85 <diegoc.spv85@gmail.com>
2024-04-10 22:13:55 -0300

    Calculadora

---
+++ CALCULADORA HTML-CSS/app.js
@@ -0,0 +1,7 @@
+function clicar(){
+    var numero1 = document.getElementById('numero1').value
+    var numero2 = document.getElementById('numero2').value
+
+    console.log(numero1)
+    console.log(numero2)
+}
