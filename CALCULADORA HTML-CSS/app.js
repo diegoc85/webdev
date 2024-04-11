c5dc66c2b1ed99274f4f942d1d0c334434f71a28
diegoc85 <diegoc.spv85@gmail.com>
2024-04-10 22:55:19 -0300

    mudancas

--- CALCULADORA HTML-CSS/app.js
+++ CALCULADORA HTML-CSS/app.js
@@ -1,7 +1,31 @@
 function clicar(){
-    var numero1 = document.getElementById('numero1').value
-    var numero2 = document.getElementById('numero2').value
+    var numero1 = parseFloat(document.getElementById('numero1').value)
+    var numero2 = parseFloat(document.getElementById('numero2').value) //parse converte
+    var resultado = document.getElementById('resultado')
+    var soma
 
-    console.log(numero1)
-    console.log(numero2)
+    function divisao (){
+        var numero1 = parseFloat(document.getElementById('numero1').value)
+        var numero2 = parseFloat(document.getElementById('numero2').value)
+        if (numero2 === o){
+            document.getElementById('resultado').innerText = 'Erro: divisão por zero'
+        }else{
+            document.getElementById('resultado').innerText = numero1 / numero2;
+        }
+    }
+
+    function multiplicacao(){
+        var numero1 = parseFloat(document.getElementById('numero1').value)
+        var numero2 = parseFloat(document.getElementById('numero2').value)
+        resultado.innerText = numero1 * numero2
+    }
+
+
+    //processamento
+    var soma = numero1 + numero2
+
+    //saida
+    console.log(soma)
+    console.log(typeof soma)
+    resultado.innerText =`O resultado da soma é ${soma}` //coloca um texto lá dentro
 }
