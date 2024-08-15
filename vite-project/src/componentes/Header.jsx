697c5e768e40a47a3827702b1e5abbea5ab57f93
diegoc85 <161664612+diegoc85@users.noreply.github.com>
2024-08-14 22:38:13 -0300

    Fim

---
+++ vite-project/src/componentes/Header.jsx
@@ -0,0 +1,14 @@
+function Header () {
+    return ( 
+        <>
+            <h1>Título da Página</h1>
+            <ul>
+                <li></li>
+                <li></li>
+            </ul>
+        </>
+        
+     );
+}
+
+export default Header;
